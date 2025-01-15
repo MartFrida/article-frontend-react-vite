c1fbea889e286d7a79823d20822a3b4159934b6f
Mary <32392607+MartFrida@users.noreply.github.com>
2025-01-15 19:07:28 +0100

    fix bug with deploy netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/article-frontend-react-vite/"
+  base: "/article-frontend-react-vite/",
+  define: {
+    __API_URL__: JSON.stringify(import.meta.env.VITE_BASE_URL),
+  },
 })
