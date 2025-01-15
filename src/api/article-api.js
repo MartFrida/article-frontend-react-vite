a9aa2ca6feabaf1d3284d6c359837e1122e29229
Mary <32392607+MartFrida@users.noreply.github.com>
2025-01-15 19:25:40 +0100

    fix bug for deploy with different destination backend and frontend

--- src/api/article-api.js
+++ src/api/article-api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const articleInstance = axios.create({
-
-  baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const fetchAllArticles = async () => {
